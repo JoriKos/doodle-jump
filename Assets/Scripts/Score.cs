9b6acd96bf5564df7b31458b2e62ee14d12ff12b
Jorikos <26204@ma-web.nl>
2022-06-14 12:43:10 +0200

    Score update

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -8,23 +8,29 @@ public class Score : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private GameObject scoreObject;
     private Rigidbody2D player;
-    private float score;
+    private float score, maxHeight;
 
     void Awake()
     {
         scoreObject = GameObject.Find("Canvas");
         scoreText = scoreObject.GetComponentInChildren<TextMeshProUGUI>();
         player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        maxHeight = 0;
         score = 0;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        if (player.velocity.y > 0)
+        if (player.velocity.y > 0 && player.transform.position.y > maxHeight)
         {
             score += player.velocity.y;
         }
+
+        if (player.transform.position.y > maxHeight)
+        {
+            maxHeight = player.transform.position.y;
+        }
+
         score = Mathf.RoundToInt(score);
         scoreText.text = "Score: " + score.ToString();
     }
